097a45e3107d3b12b3fc0b7e45d70f23eab5322a
wang-chonghuan <wangchonghuan@gmail.com>
2023-11-29 12:45:23 +0100

    new puzzles solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,15 +19,3 @@ public class Main {
     }
 }
 
-class HonoursCourse {
-    private List<Module> moduleList = new ArrayList<>();
-    public boolean addModule(Module newModule) {
-        if(moduleList.size() >= 6) {
-            return false;
-        } else {
-            moduleList.add(newModule);
-            return true;
-        }
-    }
-}
-
